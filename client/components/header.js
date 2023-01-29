c3895da7071877fb4fa61c44a57e5632fcbfdbce
klunakeskus <klunakeskus@gmail.com>
2023-01-29 16:42:37 +0300

    routes

--- client/components/header.js
+++ client/components/header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 const Header = () => {
   const [toggled, toggle] = useState(false)
 
@@ -26,8 +27,8 @@ const Header = () => {
       </div>
       {toggled && (
         <div id="menu" className="block">
-          <button
-            type="button"
+          <a
+            href="https://google.com"
             className="flex items-center px-3 py-2 border rounded text-gray-700 border-gray-700 hover:text-white hover:border-white"
           >
             <svg
@@ -35,10 +36,12 @@ const Header = () => {
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
+             
               <title>Menu</title>
+              
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
-          </button>
+          </a>
         </div>
       )}
     </nav>
